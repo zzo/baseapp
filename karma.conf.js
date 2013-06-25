2ca6592af3e1a4b39ae9bb2b8aa3c37f3c21e4f0
Mark Trostler <mark@zzo.com>
2013-06-25 14:40:03 -0700

    clean up karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,8 @@
 // Karma configuration
 // Generated on Sat Jun 01 2013 17:08:11 GMT-0700 (PDT)
 
-
 // base path, that will be used to resolve files and exclude
 basePath = '';
-
 
 // list of files / patterns to load in the browser
 files = [
@@ -23,10 +21,7 @@ preprocessors = {
 
 
 // list of files to exclude
-exclude = [
-  
-];
-
+exclude = [];
 
 // test results reporter to use
 // possible values: 'dots', 'progress', 'junit'
@@ -39,23 +34,18 @@ coverageReporter = {
 // web server port
 port = 9876;
 
-
 // cli runner port
 runnerPort = 9100;
 
-
 // enable / disable colors in the output (reporters and logs)
 colors = true;
-
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 logLevel = LOG_INFO;
 
-
 // enable / disable watching file and executing tests whenever any file changes
 autoWatch = true;
-
 
 // Start these browsers, currently available:
 // - Chrome
@@ -67,10 +57,8 @@ autoWatch = true;
 // - IE (only Windows)
 browsers = ['ChromeCanary', 'PhantomJS', 'Safari', 'Firefox'];
 
-
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 60000;
-
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
